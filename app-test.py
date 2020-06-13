4ab7079606ea6e2d71632007bf3e5944fe2a0d25
Uriel Moreno <umoreno@gmail.com>
2020-06-13 18:11:35 -0500

    escuchar ruta

--- app-test.py
+++ app-test.py
@@ -3,5 +3,9 @@ from products import products
 
 app = Flask(__name__)
 
+@app.route("/products")
+def listProducts():
+    return "Productos"
+
 if __name__ == "__main__":
     app.run(debug=True, port=4000)
