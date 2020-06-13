85cb69400e87745544a46eb156ad74e4357b8352
Uriel Moreno <umoreno@gmail.com>
2020-06-13 18:45:15 -0500

    more endpoints

--- app-test.py
+++ app-test.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
 from products import products
 
 app = Flask(__name__)
 
-@app.route('/products')
-def listProducts():
+@app.route('/test')
+def test():
     return 'Productos'
+
+@app.route('/test-json')
+def testJson():
+    return jsonify({'name': 'Productos'})
+
+@app.route('/products')
+def productList():
+    return jsonify(products)
 
 if __name__ == '__main__':
     app.run(debug=True, port=4000)
